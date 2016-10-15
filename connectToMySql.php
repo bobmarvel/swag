703c8a92413a78082640442d0155e6299c5d235b
bobmarvel <bobmarvel@yandex.ru>
2016-10-15 13:06:02 +0300

    Update connectToMySql.php

--- connectToMySql.php
+++ connectToMySql.php
@@ -4,7 +4,7 @@ $ini = new iniFile('doc2016.ini'); //create ini
 $iniArray = $ini->read();   //massive have been read
 $MySqlServer = $iniArray['MySQL_params']['MySqlServer'];
 echo $MySqlServer; 
-echo "<br>"
+echo "<br>";
 $MySqlDatabase = $iniArray['MySQL_params']['MySqlDatabase'];
 $MySqlUser = $iniArray['MySQL_params']['MySqlUser'];
 $MySqlPsw = $iniArray['MySQL_params']['MySqlPsw'];
@@ -13,6 +13,9 @@ $mysqli = mysqli_connect($MySqlServer, $MySqlUser, $MySqlPsw, $MySqlDatabase);
 if (mysqli_connect_errno()) {
 	printf("Unable to connect to MySQL: %s\%n", mysqli_connect_error());
 	exit();
+	
 }
+else echo "LULMAO";
+
 $mysqli->set_charset('utf-8');
 ?>
