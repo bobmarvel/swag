87d68d9a32c1b9407738ea8420b41448de4d6b3d
bobmarvel <bobmarvel@yandex.ru>
2016-10-29 10:59:46 +0300

    Add files via upload

---
+++ EducAdminHeader.php
@@ -0,0 +1,36 @@
+<?php
+echo <<<_PROJECTDOC
+<html>
+	<head>
+		<title> Project Doc </title>
+		<meta http-equiv="content-type" content="text/html";/>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="/style/DocProject_style.css">
+	</head>
+		<body>
+			<form method="post" action="$thePostAction"/>
+		<header>
+	
+_PROJECTDOC;
+				
+
+echo <<<_PROJECTDOC
+<table class="docProjectHeaderTable">
+			<tr>
+				<th>
+					<img class="logoSamanMati" src="/images/logoSamanMati.gif" width="144" height="80" alt="doqye">
+				</th>
+				<th >
+					<h1 class="projectName"> LAL <br />
+						<b>docich</b>
+					</h1>
+				</th>
+				<th>
+					<img class="docProjectBooks" src="/images/Books_161_100.jpg" width="161" height="100" alt="douge">
+				</th>        
+			</tr>     
+		
+	</table>
+</header>
+_PROJECTDOC;
+?>
